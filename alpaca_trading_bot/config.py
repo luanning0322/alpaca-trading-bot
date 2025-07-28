e1fc72f1e63545aa7215aff47d6e13b2167727be
luanning0322 <luanning0322@gmail.com>
2025-07-28 14:53:45 -0400

    Update config.py

--- alpaca_trading_bot/config.py
+++ alpaca_trading_bot/config.py
@@ -1,11 +1,13 @@
+# config.py
+
 ALPACA_API_KEY = "AKRSPYRQKUINR3XBZ3ER"
 ALPACA_SECRET_KEY = "j7XT3cVvrvbcq1VSXZVFodiI87sxYNr0lcNvWWCs"
 API_BASE = "https://api.alpaca.markets"
 TIMEZONE = "America/New_York"
 
 SYMBOL = "AAPL"
-INTERVAL = "1Min"        # 用于 API 请求的时间单位
-SLEEP_INTERVAL = 60      # 用于 time.sleep 的秒数（单位：秒）
+INTERVAL = "1Min"        # 用于 API 请求
+SLEEP_INTERVAL = 60      # 用于轮询等待（单位：秒）
 
 ALPACA_HEADERS = {
     "APCA-API-KEY-ID": ALPACA_API_KEY,
