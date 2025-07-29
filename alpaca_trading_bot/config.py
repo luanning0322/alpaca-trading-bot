f39a6c29f0d5485141b41e01ad662a99039e4208
luanning0322 <luanning0322@gmail.com>
2025-07-29 10:38:22 -0400

    Update config.py

--- alpaca_trading_bot/config.py
+++ alpaca_trading_bot/config.py
@@ -5,7 +5,7 @@ ALPACA_SECRET_KEY = "j7XT3cVvrvbcq1VSXZVFodiI87sxYNr0lcNvWWCs"
 API_BASE = "https://data.alpaca.markets"
 TIMEZONE = "America/New_York"
 
-SYMBOL = "AAPL"
+SYMBOL = "NVDA"
 INTERVAL = "1Min"        # 用于 API 请求
 SLEEP_INTERVAL = 60      # 用于轮询等待（单位：秒）
 
